deae1b4d0514de4c859cfdbd02812cf33702f25f
Markus George <gmarkus993@gmail.com>
2020-11-10 01:41:17 -0600

    added some javascript code

--- weightedRand.js
+++ weightedRand.js
@@ -1,18 +1,19 @@
 const spins = {
-    'red' : 3,
-    'yellow' : 2,
+    'red' : 10,
+    'yellow' : 5,
     'green' : 1
 }
 
 const spin = [];
 
-for (key in spins){
-    for (let num = spins[key]; num != 0; --num) {
-        spin.push(key);
-    };
-};
+
 
 function spinTheWheel(spin) {
+    for (key in spins){
+        for (let num = spins[key]; num != 0; --num) {
+            spin.push(key);
+        };
+    };
     return spin[Math.floor(Math.random() * spin.length)];
 };
 
